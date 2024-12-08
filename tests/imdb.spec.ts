161ee9e2e5ac3b7fd266d6e66457d5d25e52b1a3
GiovaneCalado <giovane.calado@gmail.com>
2024-12-08 19:34:11 -0300

    Stablishing connection with database

--- tests/imdb.spec.ts
+++ tests/imdb.spec.ts
@@ -1,10 +1,22 @@
 import { test, expect } from '@playwright/test';
 import { validateContract } from '../utils/apiUtils';
 import { faker } from '@faker-js/faker';
+import { Client } from 'pg';
+
+const client = new Client({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: 'imdb'
+});
 
 test.describe('Validate IMDB 100 best movies API', () => {
   test('Validate movie rate on IMDB DataBase according to non official API', async ({ request }) => {
     const response = await request.get('')
     const responseBody = await response.json()
+    await client.connect()
+    const result = await client.query(`select * from imdb_top_1000 where series_title = 'The Shawshank Redemption'`)
+    console.log(result.rows)
   });
 });
